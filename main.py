759ee3a309d3ab7b8f83ea8ca45842d5a870411f
Ask_AIC <debaishik14@gmail.com>
2024-12-10 17:21:47 -0500

    model init done

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 import gymnasium as gym
-
 from stable_baselines3 import PPO
 
-env = gym.make("Ant-v5", render_mode="human")
+env = gym.make("Ant-v5")
 print(env.action_space, env.observation_space)
 
-model = PPO("MlpPolicy", env, verbose=1, seed=0, n_steps=200, batch_size=50, device='cpu')
-print(model.policy)
-quit()
-model.learn(total_timesteps=10_000)
+n_steps = 200
+n_iterations = 1000
+
+model = PPO("MlpPolicy", env, verbose=1, seed=0, n_steps=n_steps, batch_size=50, device='cpu', tensorboard_log='logs/')
+
+# print("Starting Initial training")
+# model.learn(total_timesteps=n_iterations*n_steps, log_interval=50)
+# model.save("full_exp_on_ppo/models/ppo_ant")
+# print("Initial training done")
+# quit()
+
+print("Loading model")
+
+# Load model
+model.set_parameters("full_exp_on_ppo/models/ppo_ant", device='cpu')
+
+print("Model loaded")
 
 vec_env = model.get_env()
 obs = vec_env.reset()
-for i in range(1000):
+
+print("Starting evaluation")
+ret = 0
+for i in range(10):
     action, _states = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
-    vec_env.render()
-    # VecEnv resets automatically
-    # if done:
-    #   obs = env.reset()
+    ret += reward
+    print(f'Reward: {reward}')
 
 env.close()
