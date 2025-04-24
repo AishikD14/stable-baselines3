ea2811e753eea36f3e7f5169464de178794210c0
Ask_AIC <debaishik14@gmail.com>
2025-04-24 14:34:41 -0400

    update

--- plots/plot_total_sample_vs_reward.py
+++ plots/plot_total_sample_vs_reward.py
@@ -2,21 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-# env = "Ant-v5"
+env = "Ant-v5"
 # env = "HalfCheetah-v5"
 # env = "Hopper-v5"
 # env = "Walker2d-v5"
-env = "Humanoid-v5"
+# env = "Humanoid-v5"
 # env = "AntDir-v0"
 
-start_iteration = 1
+# start_iteration = 1
+start_iteration = 1953
 
 # Ant-v5
 # plot_list = [
-#     ["PPO_empty_space_optimal_5M_1", "Neighbor Sampling+Empty Space (gamma=0.9)"],
-#     ["PPO_empty_space_optimal_5M_2", "Neighbor Sampling+Empty Space(gamma=0.5)"],
-#     ["PPO_neighbor_search_random_walk_optimal_5M", "Neighbor Sampling+Random Walk"],
-#     ["PPO_random_search_random_walk_optimal_5M", "Random Sample+Random Walk"]
+    # ["PPO_empty_space_optimal_5M_1", "Neighbor Sampling+Empty Space (gamma=0.9)"],
+    # ["PPO_empty_space_optimal_5M_2", "Neighbor Sampling+Empty Space(gamma=0.5)"],
+    # ["PPO_neighbor_search_random_walk_optimal_5M", "Neighbor Sampling+Random Walk"],
+    # ["PPO_random_search_random_walk_optimal_5M", "Random Sample+Random Walk"]
 # ]
 
 # AntDir-v0
@@ -31,19 +32,24 @@ start_iteration = 1
 
 # Ant-v5 Less evaluations
 # plot_list = [
-#     # ["PPO_empty_space_ls_1", "PPO_empty_space with 100 iterations-300k(gamma=0.9)"],
-#     # ["PPO_empty_space_ls_2", "PPO_empty_space with 60 iterations & 3 evaluation-120k(gamma=0.9)"],
-#     # ["PPO_empty_space_ls_3", "PPO_empty_space with 60 iterations & 3 evaluation & every other point-60k(gamma=0.9)"],
-#     # ["PPO_empty_space_optimal_5M_1", "PPO_empty_space normal-1M(gamma=0.9)"],
-#     # ["PPO_empty_space_ls_7", "PPO_empty_space with 60 iterations & 3 evaluation & every other point-60k(gamma=0.5)"],
-#     # ["PPO_empty_space_optimal_5M_2", "PPO_empty_space normal-1M(gamma=0.5)"],
-#     ["PPO_empty_space_ls_8", "PPO_empty_space with 60 iterations & 3 evaluation & every other point-60k(gamma=0.3)"]
+    # ["PPO_empty_space_ls_1", "PPO_empty_space with 100 iterations-300k(gamma=0.9)"],
+    # ["PPO_empty_space_ls_2", "PPO_empty_space with 60 iterations & 3 evaluation-120k(gamma=0.9)"],
+    # ["PPO_empty_space_ls_3", "PPO_empty_space with 60 iterations & 3 evaluation & every other point-60k(gamma=0.9)"],
+    # ["PPO_empty_space_optimal_5M_1", "PPO_empty_space normal-1M(gamma=0.9)"],
+    # ["PPO_empty_space_ls_7", "PPO_empty_space with 60 iterations & 3 evaluation & every other point-60k(gamma=0.5)"],
+    # ["PPO_empty_space_optimal_5M_2", "PPO_empty_space normal-1M(gamma=0.5)"],
+    # ["PPO_empty_space_ls_8", "PPO_empty_space with 60 iterations & 3 evaluation & every other point-60k(gamma=0.3)"],
 # ]
 
-# Other envs
+# Ant-v5 FQE evaluations
 plot_list = [
-    ["PPO_empty_space", "PPO_empty_space with 60 iterations & 3 evaluation & every other point-60k(gamma=0.3)"],
+    ["PPO_empty_space_ls_10", "Advantage estimation using top 5 agent and 2 evaluation- 10k; gamma=0.3", "adv_results"],
 ]
+
+# Other envs
+# plot_list = [
+#     ["PPO_empty_space", "PPO_empty_space with 60 iterations & 3 evaluation & every other point-60k(gamma=0.3)"],
+# ]
 
 plot_metrics = []
 
@@ -54,10 +60,11 @@ for plot_item in plot_list:
     print("------------------------------------")
     print("Working on "+plot_item[0]+" directory")
     reward_values = []
+    searchString = plot_item[2] if len(plot_item) > 2 else "results"
     try:
         for filename in os.listdir(directory):
             # Check if the filename starts with "results"
-            if filename.startswith('results'):
+            if filename.startswith(searchString):
                 # Load the rewards
                 results = np.load(directory + "/" + filename)
                 # Find the maximum reward
