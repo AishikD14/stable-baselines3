75a8589b3289066692ccc81d43a37e75212ff748
Ask_AIC <debaishik14@gmail.com>
2025-04-29 00:33:27 -0400

    update

--- plots/plot_total_sample_vs_reward.py
+++ plots/plot_total_sample_vs_reward.py
@@ -44,10 +44,15 @@ start_iteration = 1953
 # Ant-v5 FQE evaluations
 plot_list = [
     ["PPO_empty_space_ls_11", "Advantage estimation using top 5 agent and 2 evaluation & new fqe- 10k; gamma=0.3"],
-    # ["PPO_test_1", "Normal training"],
-    # ["PPO_test_2", "Normal training with batch_size=657"],
-    # ["PPO_empty_space_ls_1", "Advantage estimation using top 5 agent and 2 evaluation & new fqe- 10k; gamma=0.3"],
+    ["PPO_empty_space_ls_8", "PPO_empty_space with 60 iterations & 3 evaluation & every other point-60k(gamma=0.3)"],
+    ["PPO_test_1", "Baseline with batch_size=32"],
+    ["PPO_test_2", "Baseline with batch_size=657"],
 ]
+
+# HalfCheetah-v5 FQE evaluations
+# plot_list = [
+#     ["PPO_empty_space_ls_1", "Advantage estimation using top 5 agent and 2 evaluation & new fqe- 10k; gamma=0.3"],
+# ]
 
 # Baseline
 # plot_list = [
