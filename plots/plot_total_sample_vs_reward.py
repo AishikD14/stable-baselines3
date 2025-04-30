f245aecdd318d0e1d5ac749407a6fcceaafbe545
Ask_AIC <debaishik14@gmail.com>
2025-04-30 01:35:19 -0400

    update

--- plots/plot_total_sample_vs_reward.py
+++ plots/plot_total_sample_vs_reward.py
@@ -43,23 +43,25 @@ start_iteration = 1953
 
 # Ant-v5 FQE evaluations
 # plot_list = [
-#     ["PPO_empty_space_ls_11", "FQE top 5 agent & 2 eval & search=1- 10k; gamma=0.3"],
-#     ["PPO_empty_space_ls_12", "FQE top 5 agent & 2 eval & search=2- 10k; gamma=0.3"],
+    # ["PPO_empty_space_ls_11", "FQE top 5 agent & 2 eval & search=1 -10k; gamma=0.3"],
+    # ["PPO_baseline_1", "Baseline with search=1 -10k"],
+    # ["PPO_empty_space_ls_12", "FQE top 5 agent & 2 eval & search=2- 10k; gamma=0.3"],
+    # ["PPO_baseline_2", "Baseline with search=2 -10k"],
 #     ["PPO_empty_space_ls_13", "FQE top 5 agent & 2 eval & search=3- 10k; gamma=0.3"],
-#     ["PPO_empty_space_ls_8", "Online eval with 60 iter & 3 eval & every other point-60k; gamma=0.3"],
-#     ["PPO_baseline_1", "Baseline with batch_size=657"],
+#     ["PPO_baseline_3", "Baseline with search=3 -10k"],
+#     ["PPO_empty_space_ls_8", "Online eval with 60 iter & 3 eval & every other point -60k; gamma=0.3"], # Upper bound
 # ]
 
 # HalfCheetah-v5 FQE evaluations
-plot_list = [
-    ["PPO_empty_space_ls_1", "FQE top 5 agent and 2 eval & search=1- 10k; gamma=0.3"],
-    # ["PPO_empty_space", "Online eval with 60 iter & 3 eval & every other point-60k; gamma=0.3"],
-    # ["PPO_baseline_1", "Baseline with batch_size=657"],
-]
+# plot_list = [
+#     ["PPO_empty_space_ls_1", "FQE top 5 agent and 2 eval & search=1- 10k; gamma=0.3"],
+#     ["PPO_baseline_1", "Baseline with search=1 -10k"],
+#     ["PPO_empty_space", "Online eval with 60 iter & 3 eval & every other point-60k; gamma=0.3"], # Upper bound
+# ]
 
 # Baseline
 # plot_list = [
-#     ["PPO_baseline_1", "Baseline with batch_size=657"],
+#     ["PPO_baseline_1", "Baseline with search=1 -10k"],
 # ]
 
 plot_metrics = []
