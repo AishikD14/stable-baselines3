59697799898312c2e56378f56e2ca09415ea27c4
Ask_AIC <debaishik14@gmail.com>
2025-09-23 23:22:09 -0400

    update

--- plots/combine_seeds_time.py
+++ plots/combine_seeds_time.py
@@ -10,8 +10,8 @@ from data_collection_config import args_ant, args_half_cheetah, args_walker2d, a
 parser = argparse.ArgumentParser()
 args, rest_args = parser.parse_known_args()
 
-env = "Ant-v5"
-# env = "HalfCheetah-v5"
+# env = "Ant-v5"
+env = "HalfCheetah-v5"
 # env = "Hopper-v5"
 # env = "Walker2d-v5"
 # env = "Humanoid-v5"
@@ -54,7 +54,7 @@ seed_list = [0, 1, 2, 3]
 
 plot_list = [
     # ["PPO_FQE", "PPO FQE with 60 iterations & every other point; gamma=0.3"],
-    ["PPO_normal_training", "PPO Normal Training"],
+    # ["PPO_normal_training", "PPO Normal Training"],
     # ["PPO_upper_bound", "PPO Upper Bound"],
     # ["TRPO_normal_training", "TRPO Normal Training"],
     # ["TRPO_upper_bound", "TRPO Upper Bound"],
@@ -72,6 +72,8 @@ plot_list = [
     # ["PPO_empty_space_ls", "PPO FQE"],
     # ["PPO_baseline", "PPO Baseline"],
     # ["PPO_Rebuttal_1", "PPO Rebuttal 1"],
+    # ["TRPO_PBT", "TRPO Population Based Training"],
+    ["TRPO_CheckpointAvg", "TRPO Checkpoint averaging"],
 ]
 
 plot_metrics = []
